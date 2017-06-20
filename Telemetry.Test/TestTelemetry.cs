eeb8015c973f17e9ad31f0429a9f71f5ee792506
Alexander Korsunsky <fat.lobyte9@gmail.com>
2017-06-20 18:44:56 +0200

    analysis: Add example analysis Jupyter notebook

--- Telemetry.Test/TestTelemetry.cs
+++ Telemetry.Test/TestTelemetry.cs
@@ -12,7 +12,9 @@ namespace Telemetry.Test
         public void Awake()
         {
             TelemetryService.Instance.CreateChannel("ut", typeof(double));
-            TelemetryService.Instance.CreateChannel("altitude", typeof(double));
+            TelemetryService.Instance.CreateChannel("altitude", typeof(double), "0000");
+            TelemetryService.Instance.CreateChannel("srf_speed", typeof(double), "0000.0");
+            TelemetryService.Instance.CreateChannel("static_pressure", typeof(double));
             TelemetryService.Instance.CreateChannel("partcount", typeof(int));
         }
 
@@ -24,6 +26,11 @@ namespace Telemetry.Test
 
             TelemetryService.Instance.Send("ut", ut);
             TelemetryService.Instance.Send("altitude", FlightGlobals.ship_altitude);
+            TelemetryService.Instance.Send("srf_speed", FlightGlobals.ship_srfSpeed);
+
+            double pressure = FlightGlobals.getStaticPressure();
+            if (pressure != 0.0d)
+            TelemetryService.Instance.Send("static_pressure", pressure);
 
             if (lastPartCount != FlightGlobals.ActiveVessel.parts.Count)
             {
